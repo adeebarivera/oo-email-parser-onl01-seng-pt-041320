0cc3d44f1cda54a37701c097a37211b0ea9d5d97
adeeba reaz <adeebarivera@outlook.com>
2020-05-11 20:45:09 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,12 +2,12 @@ class EmailAddressParser
   attr_accessor :name, :cvs_email
   
   def initialize(cvs_email)
-    @emails = cvs_email
+    @cvs_email = cvs_email
   end
 
   def parse
-    emails.split.map do |email|
-      email.split(',')
-    end.flatten.uniq
+    cvs_email.split.collect do |address| address.split(',')
+    end
+   .flatten.uniq
   end
 end
